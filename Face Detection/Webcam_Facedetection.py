e08f1795e43ed849de89cdab9ba33b287d7146af
Midhru Jayan J <midhru004@gmail.com>
2024-12-10 19:14:04 +0530

    Using webcam also done

--- Face Detection/Webcam_Facedetection.py
+++ Face Detection/Webcam_Facedetection.py
@@ -1,9 +1,6 @@
 import cv2
-
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-
 cap = cv2.VideoCapture(0)
-
 while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -16,6 +13,5 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 cap.release()
 cv2.destroyAllWindows()
